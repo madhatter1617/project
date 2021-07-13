27df6539f086b548d8e6b439f081ca92e39511d6
Rosa <rosarenee@att.net>
2021-07-13 09:19:49 -0500

    got the basic links with fetch

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-//idk yet
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("test biss");
+    margarita()
+    mojito()
+    tea()
+    ginGin()
+    vodka()
+    triSec()    
+  });
+function margarita() { //drinks involved with margarita
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  .then(response => response.json())
+  .then(marg => console.log(marg));
+}  
+
+
+function mojito() { //drinks involved with mojito
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
+      .then(response => response.json())
+      .then(marg => console.log(marg));
+    } 
+
+function tea() { //drinks involved with tea
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=tea')
+    .then(response => response.json())
+    .then(marg => console.log(marg));
+        } 
+
+function ginGin() { //drinks involved with the Ingredient  gin
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
+      .then(response => response.json())
+      .then(gin => console.log(gin));
+    } 
+
+function vodka() { //drinks involved with the Ingredient  vodka
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka')
+    .then(response => response.json())
+    .then(gin => console.log(gin));
+    } 
+
+function triSec() { //drinks involved with the Ingredient  triple sec
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple%20sec')
+    .then(response => response.json())
+    .then(gin => console.log(gin));
+        } 
+    
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-wonderland
+Users will be able to mouseOver the images provided and will see 1 random recipe relating to the image or title of the image.
+I would like to have 6 to 9 images. 
+I would like a few to be more as an ingredient to a drink and 3 recipes will pop up relating to it. 
+
+The user will be able to click on a "random" button and a random drink will appear with the title, image, and small description.
+
+I’ll be using the website and the API that is provided to get my information: https://www.thecocktaildb.com/api.php
+
+EventListeners: 
+Random button click 
+MouseOver on the images
