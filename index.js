1f212647a7ccd859cfb1be007dcd9cfcb305c950
Rosa <rosarenee@att.net>
2021-07-13 15:53:41 -0500

    got basic set up

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("test biss");
     margarita()
-    mojito()
-    tea()
-    ginGin()
-    vodka()
-    triSec()    
+    //vodka()
+      
   });
 function margarita() { //drinks involved with margarita
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
   .then(response => response.json())
-  .then(marg => console.log(marg));
+  .then(data => margBtn(data));
+
 }  
 
+function margBtn (data){
+    console.log(data)
+    let marg = document.querySelector('#marg')
+    //marg.textContent = data[1]
+    console.log(data)
+}
 
-function mojito() { //drinks involved with mojito
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
-      .then(response => response.json())
-      .then(marg => console.log(marg));
-    } 
 
-function tea() { //drinks involved with tea
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=tea')
-    .then(response => response.json())
-    .then(marg => console.log(marg));
-        } 
 
-function ginGin() { //drinks involved with the Ingredient  gin
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
-      .then(response => response.json())
-      .then(gin => console.log(gin));
-    } 
-
-function vodka() { //drinks involved with the Ingredient  vodka
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka')
-    .then(response => response.json())
-    .then(gin => console.log(gin));
-    } 
-
-function triSec() { //drinks involved with the Ingredient  triple sec
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple%20sec')
-    .then(response => response.json())
-    .then(gin => console.log(gin));
-        } 
-    
+// function vodka() { //drinks involved with the Ingredient  vodka
+//     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka')
+//     .then(response => response.json())
+//     .then(vodka => {
+//         //debugger;
+//     });
+//     } 
